6d2e333f84e8e32f0470b54ae4ae9569cd5a4bf2
ProkhorovKV <ProkhorovKV@Piteravto.ru>
2022-01-24 13:52:42 +0300

    Считан QR Code с путевого листа.

--- app/src/main/java/ru/piteravto/barcodescanner/scanner/BarcodeAnalyzer.kt
+++ app/src/main/java/ru/piteravto/barcodescanner/scanner/BarcodeAnalyzer.kt
@@ -3,7 +3,9 @@ package ru.piteravto.barcodescanner.scanner
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
 private const val TAG = "BarcodeAnalyzer"
@@ -11,17 +13,30 @@ private const val TAG = "BarcodeAnalyzer"
 /** Класс для обработки захваченного ImageAnalysis изображения
  * все данные постыпают в analyze где их и можно обрабатывать */
 class BarcodeAnalyzer : ImageAnalysis.Analyzer {
-    private val scanner = BarcodeScanning.getClient()
+
 
     override fun analyze(image: ImageProxy) {
-        val rotationDegrees = image.imageInfo.rotationDegrees
-        Log.e(TAG, "analyze: $rotationDegrees ")
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_AZTEC,
+                Barcode.FORMAT_DATA_MATRIX
+            )
+            .build()
+        val scanner = BarcodeScanning.getClient(options)
 
-        val barcodeImage = InputImage.fromMediaImage(image.image, image.imageInfo.rotationDegrees)
+        val rotationDegrees = image.imageInfo.rotationDegrees
+
+        val barcodeImage = InputImage.fromMediaImage(image.image, rotationDegrees)
 
         val result = scanner.process(barcodeImage)
-            .addOnSuccessListener { Log.e(TAG, "success") }
-            .addOnFailureListener { Log.e(TAG, "failure") }
+            .addOnSuccessListener { barcodes ->
+                barcodes.forEach {
+                    if (it.valueType == Barcode.TYPE_TEXT) {
+                        Log.e(TAG, "analyze: ${it.displayValue}")
+                    }
+                }
+            }
         // after done, release the ImageProxy object
         image.close()
     }
